7b0db924032d869877398894b264156c64ac6d7b
katerin <ckaterinarellano@gmail.com>
2023-02-07 11:32:15 -0500

    feature: agregar componente slider

--- src/hook/use-news.tsx
+++ src/hook/use-news.tsx
@@ -5,19 +5,22 @@ import { Article } from '../interface/interface';
 export const useArticle = () => {
     const [loading, setloading] = useState(true);
     const [articles, setArticles ] = useState<Article[]>([]);
+    const [listArticles, setListArticles] = useState<Article[]>([]);
     
     const getNote = async() => {
         const data:Article[] = await getNews();
+        const dataArticles:Article[] = data.slice(0,4);
         
         setArticles(data);
+        setListArticles(dataArticles);
         setloading(false);
     }
     useEffect(() => {
       getNote();
     }, [])
-
     return {
         loading,
         articles,
+        listArticles,
     }
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,24 @@
 import { useArticle } from '../hook/use-news';
+import { Slider } from '../components/slider/slider';
+
+const classes = {
+    content: 'content'
+}
 
 export const Home = () => {
     const {articles} = useArticle();
     return (
-        <section>
-            {articles.map(({title,content}) => (
-                <div key={title}>
-                    <h2>{title}</h2>
-                    <p>{content}</p>
-                </div>
-            ))}
+        <section className={classes.content}>
+            {/* <section>
+                {articles.map(({title,content}) => (
+                    <div key={title}>
+                        <h2>{title}</h2>
+                        <p>{content}</p>
+                    </div>
+                ))}
+            </section> */}
+            <Slider/>     
         </section>
+        
     )
 }
