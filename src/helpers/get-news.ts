9da3791a0e883265927e87d3b0c73433dca0871b
Sharmelyn Violeta <sbautistal@ulasalle.edu.pe>
2023-02-11 01:58:47 -0500

    Feature - creando componente suscription

--- src/helpers/get-news.ts
+++ src/helpers/get-news.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apikey = process.env.REACT_APP_API_KEY;
-const baseUrl = `https://newsapi.org/v2/top-headlines?country=mx&apiKey=${apikey}`;
+const baseUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apikey}`;
 
 
 export const getNews = async(category:string = 'general',pageSize:number=20, query:string = '') => {
